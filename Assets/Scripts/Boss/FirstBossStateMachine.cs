aed4373f6bf3be76753f0d9c0b895c933aeddd0b
bsari12 <berkaysari1225@gmail.com>
2026-01-03 21:33:31 +0300

    Boss Idle & Teleport Added

--- Assets/Scripts/Boss/FirstBossStateMachine.cs
+++ Assets/Scripts/Boss/FirstBossStateMachine.cs
@@ -2,5 +2,110 @@ using UnityEngine;
 
 public class FirstBossStateMachine : BossStateMachine
 {
-    
+    [SerializeField] BossPhysics bossPhysics;
+    private Player player;
+
+    [Header("IDLE STATE")]
+    [SerializeField] private string idleAnimationName;
+    [SerializeField] private float minIdleTime;
+    [SerializeField] private float maxIdleTime;
+    private float idleStateTimer;
+
+    [Header("TELEPORT STATE")]
+    [SerializeField] private string teleportOutAnimationName;
+    [SerializeField] private string teleportInAnimationName;
+    [SerializeField] private float minTeleportTime;
+    [SerializeField] private float maxTeleportTime;
+    [SerializeField] private Transform[] teleportPoints;
+    private float teleportStateTimer;
+    private int teleportIndex;
+    private int lastTeleportIndex;
+    private bool canCheckTeleportInfo;
+
+    void Start()
+    {
+        player = FindAnyObjectByType<Player>();
+    }
+    #region IDLE
+    public override void EnterIdle()
+    {
+        anim.Play(idleAnimationName);
+        idleStateTimer =Random.Range(minIdleTime,maxIdleTime);
+    }
+
+    public override void UpdateIdle()
+    {
+        idleStateTimer -= Time.deltaTime;
+        if(idleStateTimer <= 0)
+        {
+            ChangeState(BossState.Teleport);
+        }
+
+    }
+    #endregion
+
+    #region TELEPORT
+
+    public override void EnterTeleport()
+    {
+        bossPhysics.DisableStatsCol();
+        teleportIndex = Random.Range(0,teleportPoints.Length);
+        while(teleportIndex == lastTeleportIndex)
+        {
+            teleportIndex = Random.Range(0,teleportPoints.Length);
+        }
+        lastTeleportIndex = teleportIndex;
+        anim.Play(teleportOutAnimationName);
+    }
+
+    public override void UpdateTeleport()
+    {
+        if(!canCheckTeleportInfo)
+            return;
+        
+        teleportStateTimer -= Time.deltaTime;
+        if (bossPhysics.inAttackRange)
+        {
+            ChangeState(BossState.Idle);
+        }
+        else if (teleportStateTimer <= 0)
+        {
+            ChangeState(BossState.Idle);
+        }
+
+    }
+    public override void ExitTeleport()
+    {
+        canCheckTeleportInfo = false;
+    }
+    public void Teleport()
+    {
+        int randomChance = Random.Range(0, 2);
+        if(randomChance == 0)
+        {
+            transform.position = teleportPoints[teleportIndex].position;
+        }
+        else
+        {
+            if(player!=null)
+                transform.position = player.transform.position + Vector3.up *1.6f;
+            else
+                transform.position = teleportPoints[teleportIndex].position;
+        }
+        anim.Play(teleportInAnimationName);
+    }
+
+    public void EnableCheckingTeleport()
+    {
+        canCheckTeleportInfo = true;
+        teleportStateTimer = Random.Range(minTeleportTime,maxTeleportTime);
+        bossPhysics.EnableStatsCol();
+        bossPhysics.EnableDetectionCol();
+        anim.Play(idleAnimationName);
+    }
+
+    #endregion
+
+
+
 }
